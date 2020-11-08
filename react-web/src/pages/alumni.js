e4f1acbc30194410b72059549bfd837ab3e35f20
Aditya Thakur <aditya.thakur.261099@gmail.com>
2020-11-08 17:14:25 +0530

    chore: Skeletal Structure for Panel Components initialized!

---
+++ react-web/src/pages/alumni.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Alumni extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <>
+                <h2> Alumni </h2>
+            </>
+        );
+    }
+}
+
+export default Alumni;
---
+++ react-web/src/pages/forum.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Forum extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <>
+                <h2> Forum </h2>
+            </>
+        );
+    }
+}
+
+export default Forum;
