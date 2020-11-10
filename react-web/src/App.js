6144cfa3c47d5e23ac77d4e5a0cf5ff5cad167e6
Aditya Thakur <aditya.thakur.261099@gmail.com>
2020-11-10 18:46:04 +0530

    feat: React Router Navigation Added!

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,201 +1,45 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import EventIcon from '@material-ui/icons/Event';
-import ForumIcon from '@material-ui/icons/Forum';
-import PersonIcon from '@material-ui/icons/Person';
-import NotesIcon from '@material-ui/icons/Notes';
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Post from './components/post';
+import DashBoard from './dashboard';
+import Login from './login';
 
-const drawerWidth = 240;
+class App extends React.Component {
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
+    constructor(props) {
+        super(props);
+        // TODO : Toggle login var below to change auth state
+        let login = true;
+        this.state = {
+          login : login
+        }
+    }
 
-export default function MiniDrawer() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+    // TODO : Fetch User Session Cookies
+    fetchCookies = function() {
+      return;
+    }
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+    // Change Login state
+    changeLogin = (loginState)=> {
+      this.setState({
+        login : loginState
+      });
+    }
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            UNILLEGE
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </div>
-        
+    render() {
 
-      <Divider />
-      <List>
-        
-          <ListItem button key="dashboard">
-            <ListItemIcon>
-              <DashboardIcon/>
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-          </ListItem>
-          
-          <ListItem button key="events">
-            <ListItemIcon>
-              <EventIcon/>
-            </ListItemIcon>
-            <ListItemText primary="Events" />
-          </ListItem>
+        const renderer = this.state.login?
+                          <DashBoard listener={this.changeLogin}/> :
+                          <Login listener={this.changeLogin} />;
 
-          <ListItem button key="posts">
-            <ListItemIcon>
-              <ForumIcon/>
-            </ListItemIcon>
-            <ListItemText primary="Forum" />
-          </ListItem>
-        
-      </List>
-      <Divider />
-      <List>
-        
-          <ListItem button key='alumni'>
-            <ListItemIcon>
-              <PersonIcon/>
-            </ListItemIcon>
-            <ListItemText primary="Alumni" />
-          </ListItem>
-
-          <ListItem button key='preparation'>
-            <ListItemIcon>
-              <NotesIcon/>
-            </ListItemIcon>
-            <ListItemText primary='Preparation' />
-          </ListItem>
-        
-      </List>
-    
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-
-        <Post post_id='1' post='Hola Amigos' user_id='Aditya Thakur' timestamp='Nov 8,2020' image='https://live.staticflickr.com/106/299768558_02779187fe_b.jpg' votes='12' />
-
-      </main>
-    </div>
-  );
+        return (
+          <Router>
+            { renderer }
+          </Router>
+        )
+    }
 }
+
+export default App;
