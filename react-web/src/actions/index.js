2244a0750b2a7d08f33410884b46f40f6772fae3
Shubham Luthra <luthra2059@gmail.com>
2020-11-15 11:56:27 +0530

    feat: Linked frontend to backend user management logic

---
+++ react-web/src/actions/index.js
@@ -0,0 +1,4 @@
+import * as auth from "./auth";
+import * as posts from "./posts";//todo
+
+export {posts, auth}
---
+++ react-web/src/reducers/index.js
@@ -0,0 +1,4 @@
+import auth from "./auth";
+const unillegeApp = combineReducers({
+    posts, auth
+})
