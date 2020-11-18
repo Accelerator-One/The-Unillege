37fb1f5101ca46049bfa33df2702187cc73ca887
Shubham Luthra <luthra2059@gmail.com>
2020-11-18 17:31:15 +0530

    patch: UI fixes!

--- react-web/src/pages/feed.js
+++ react-web/src/pages/feed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Post from '../components/post';
+import Grid from '@material-ui/core/Grid';
 
 class DashboardFeed extends React.Component {
 
@@ -10,14 +11,14 @@ class DashboardFeed extends React.Component {
 
     render(){
         return (
-            <>
+            <Grid container spacing={4} style={{ 'padding':'30px' }}>
             {
                 this.props.use.posts.map(post=>(
-                    <Post post_id={post.id} post={post.title} user_id={post.author_id} timestamp={post.created_on} image='https://live.staticflickr.com/106/299768558_02779187fe_b.jpg' votes='12' />
+                    <Post spacing={10} key={post.id} post={post.title} user_id={post.author_id} timestamp={post.created_on} image='https://live.staticflickr.com/106/299768558_02779187fe_b.jpg' votes='12' />
                 ))
             }
                 
-            </>
+            </Grid>
         );
     }
 }
