a1bebf2bc1e7da86168b07565cb426c7735b7084
Aditya Thakur <aditya.thakur.261099@gmail.com>
2020-11-18 17:10:05 +0530

    patch: fixed grid in dashboard feed!

--- react-web/src/pages/feed.js
+++ react-web/src/pages/feed.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import Post from '../components/post';
 
 class DashboardFeed extends React.Component {
@@ -10,9 +11,10 @@ class DashboardFeed extends React.Component {
 
     render(){
         return (
-            <>
-                <Post post_id='1' post='Hola Amigos' user_id='Aditya Thakur' timestamp='Nov 8,2020' image='https://live.staticflickr.com/106/299768558_02779187fe_b.jpg' votes='12' />
-            </>
+            <Grid container spacing={4} style={{ 'padding':'20px' }}>
+                <Post post_id='1' post='Hola Amigos' user_id='Aditya Thakur'   spacing={4} timestamp='Nov 8,2020' image='https://live.staticflickr.com/106/299768558_02779187fe_b.jpg' votes='12' />
+                <Post post_id='1' post='Hola Amigos' user_id='Aditya Thakur'   spacing={4} timestamp='Nov 8,2020' image='https://live.staticflickr.com/106/299768558_02779187fe_b.jpg' votes='12' />
+            </Grid>
         );
     }
 }
