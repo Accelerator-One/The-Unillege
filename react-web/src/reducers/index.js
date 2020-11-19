479e88a0cfe45cdbfd39908c67522d03715d7ae5
Shubham Luthra <luthra2059@gmail.com>
2020-11-19 15:27:56 +0530

    minor changes

--- react-web/src/reducers/index.js
+++ react-web/src/reducers/index.js
@@ -1,9 +1,11 @@
 import auth from "./auth";
 import posts from "./posts";
+import alumni from "./alumni";
+import events from "./events";
 import {combineReducers} from "redux";
 
 const unillegeApp = combineReducers({
-    posts, auth
+    posts, auth, events, alumni
 })
 
 export default unillegeApp;
